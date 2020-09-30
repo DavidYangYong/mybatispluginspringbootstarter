8f5172103bed240663ab4d8a097b0b3ddd5dba0d
DavidYangYongZhi <245317989@qq.com>
2020-09-30 14:47:19 +0800

    spring boot test

--- src/test/java/com/fl/ServiceConfigApplicationTests.java
+++ src/test/java/com/fl/ServiceConfigApplicationTests.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {FlMybatisPluginAutoConfiguration.class})
 @TestPropertySource("classpath:application.yml")
 public class ServiceConfigApplicationTests {
