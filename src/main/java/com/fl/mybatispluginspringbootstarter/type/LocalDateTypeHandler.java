7ef794132d47520242397bfc3aa0677e6b28b905
DavidYangYongZhi <245317989@qq.com>
2021-06-04 13:52:41 +0800

    spring boot test

--- src/main/java/com/fl/mybatispluginspringbootstarter/type/LocalDateTypeHandler.java
+++ src/main/java/com/fl/mybatispluginspringbootstarter/type/LocalDateTypeHandler.java
@@ -4,6 +4,7 @@ import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.joda.time.LocalDate;
@@ -20,21 +21,37 @@ public class LocalDateTypeHandler extends BaseTypeHandler<LocalDate> {
 	@Override
 	public void setNonNullParameter(PreparedStatement ps, int i, LocalDate parameter, JdbcType jdbcType)
 			throws SQLException {
-		ps.setObject(i, parameter);
+		Date date = parameter.toDate();
+		ps.setObject(i, date);
 	}
 
 	@Override
 	public LocalDate getNullableResult(ResultSet rs, String columnName) throws SQLException {
-		return (LocalDate) rs.getObject(columnName, LocalDate.class);
+		Date date = rs.getDate(columnName);
+		if (date != null) {
+			return new LocalDate(date);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public LocalDate getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-		return (LocalDate) rs.getObject(columnIndex, LocalDate.class);
+		Date date = rs.getDate(columnIndex);
+		if (date != null) {
+			return new LocalDate(date);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public LocalDate getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-		return (LocalDate) cs.getObject(columnIndex, LocalDate.class);
+		Date date = cs.getDate(columnIndex);
+		if (date != null) {
+			return new LocalDate(date);
+		} else {
+			return null;
+		}
 	}
 }
